68d3389f330b7419a33563519fb65957ce884f28
Samith Sandanayake <sam.the.devel@gmail.com>
2011-06-27 11:29:19 +0530

    Preview Generation classes added

---
+++ src/lk/floss/moe/mqe/preview/MoodleHTML.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.floss.moe.mqe.preview;
+
+/**
+ *
+ * @author sam
+ */
+public class MoodleHTML {
+    
+}
